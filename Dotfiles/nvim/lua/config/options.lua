cb42184cea2fbd3a15387fc87988f3766dec6d15
snoooooooope <holycityvl@gmail.com>
2024-04-28 13:16:26 -0400

    nvim files

---
+++ Dotfiles/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.relativenumber = true
+vim.g.have_nerd_font = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
---
+++ Dotfiles/nvim/lua/plugins/oxocarbon.lua
@@ -0,0 +1,12 @@
+return {
+  -- add oxocarbon
+  { "nyoom-engineering/oxocarbon.nvim" },
+
+  -- Configure LazyVim to load oxocarbon
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon",
+    },
+  },
+}
